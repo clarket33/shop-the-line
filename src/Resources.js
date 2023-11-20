eaf7a916592352bf2f7a4bc48fdde056764b8748
Thomas Clarke <clarket33@gmail.com>
2023-11-20 17:47:53 -0500

    add new markets

--- src/Resources.js
+++ src/Resources.js
@@ -20,7 +20,7 @@ export const player_prop_markets = [
       label: "baseball_mlb"
     },
     {
-      player_markets: "player_points,player_power_play_points,player_assists,player_blocked_shots,player_shots_on_goal",
+      player_markets: "player_points,player_power_play_points,player_assists,player_blocked_shots,player_shots_on_goal,player_total_saves,player_goal_scorer_first,player_goal_scorer_last,player_goal_scorer_anytime",
       team_markets: "alternate_spreads,h2h_p1,h2h_p2,h2h_p3,spreads_p1,spreads_p2,spreads_p3,totals_p1,totals_p2,totals_p3,",
       label: "icehockey_nhl"
     }
@@ -30,7 +30,7 @@ export const additional_team_props = new Set(["alternate_spreads","h2h_q1","h2h_
                                               "h2h_p1","h2h_p2","h2h_p3","spreads_p1","spreads_p2","spreads_p3","totals_p1","totals_p2","totals_p3"]);
 export const all_player_props = new Set(["player_pass_tds","player_pass_yds","player_pass_completions","player_pass_attempts","player_pass_interceptions","player_pass_longest_completion","player_rush_yds","player_rush_attempts","player_rush_longest","player_receptions","player_reception_yds","player_reception_longest","player_kicking_points","player_field_goals","player_tackles_assists","player_1st_td","player_last_td","player_anytime_td","player_points","player_rebounds","player_assists","player_threes","player_double_double",
                                           "player_blocks","player_steals","player_turnovers","player_points_rebounds_assists","player_points_rebounds","player_points_assists","player_rebounds_assists","batter_home_runs","batter_hits","batter_total_bases","batter_rbis","batter_runs_scored","batter_hits_runs_rbis","batter_singles","batter_doubles","batter_triples","batter_walks","batter_strikeouts","batter_stolen_bases","pitcher_strikeouts",
-                                          "pitcher_record_a_win","pitcher_hits_allowed","pitcher_walks","pitcher_earned_runs","pitcher_outs","player_power_play_points","player_blocked_shots","player_shots_on_goal"])
+                                          "pitcher_record_a_win","pitcher_hits_allowed","pitcher_walks","pitcher_earned_runs","pitcher_outs","player_power_play_points","player_blocked_shots","player_shots_on_goal,player_total_saves,player_goal_scorer_first,player_goal_scorer_last,player_goal_scorer_anytime"])
 
 export const league_titles = {
   "americanfootball_nfl":"NFL",
@@ -95,7 +95,12 @@ export const player_prop_choices = {
   //hockey
   "player_power_play_points":"Power Play Points",
   "player_blocked_shots":"Blocked Shots",
-  "player_shots_on_goal":"Shots on Goal"
+  "player_shots_on_goal":"Shots on Goal",
+  "player_total_saves":"Total Saves",
+  "player_goal_scorer_first":"First Goal Scorer",
+  "player_goal_scorer_last":"Last Goal Scorer",
+  "player_goal_scorer_anytime":"Anytime Goal Scorer"
+
 }
 
 export const additional_team_prop_titles={ 
