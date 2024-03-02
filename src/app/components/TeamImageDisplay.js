8727a95193595639a5c0beef65e861d8d4de24f8
Thomas Clarke <clarket33@gmail.com>
2024-03-02 15:54:14 -0500

    add sharp

--- src/app/components/TeamImageDisplay.js
+++ src/app/components/TeamImageDisplay.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react'
 
 export default function TeamImageDisplay(imageProperties) {
     const [imageSrc, setImageSRC] = useState(imageProperties.src);
-    if(!imageSrc) return (<></>);
 
     return (
         <Image
