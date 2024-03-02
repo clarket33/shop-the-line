70ea34eb7f86ec43194c06517530629b333aac99
Thomas Clarke <clarket33@gmail.com>
2024-03-02 14:47:56 -0500

    chnage spinner colors

--- src/app/components/TeamPropDisplay.js
+++ src/app/components/TeamPropDisplay.js
@@ -342,7 +342,7 @@ const TeamPropDisplay = (game) => {
     }else{
         return (
             <div className="flex flex-wrap justify-center items-center">
-                {status === "loading" ? <Spinner data-testid="loader" className="h-8 w-8" />:
+                {status === "loading" ? <Spinner data-testid="loader" color="blue" className="h-8 w-8" />:
                 status === "error" ? <span className="text-red-500 font-bold text-sm text-center">An unexpected error has occurred. Please try again later</span>:<></> }
             </div>
         )
